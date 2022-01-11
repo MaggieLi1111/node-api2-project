afe2ad45186000f4541c2833d837baf8c022d4d1
unknown <lidanyang1111@gmail.com>
2022-01-11 13:28:13 -0800

    version1

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // require your server and launch it here
+const server = require("./api/server.js");
+
+const Port = 5000;
+
+server.listen(Port, () => {
+    console.log(`Listening on localhost:${Port}`);
+})
